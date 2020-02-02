ccce460326c66755a312ecb85a90665804032964
Denis <Rjek051@gmail.com>
2020-02-02 15:31:20 +0200

    first commit

--- SUDOKA/Source.cpp
+++ SUDOKA/Source.cpp
@@ -52,7 +52,7 @@ void Sudoku(int** a)
 			for (int h = 1; h < 10; ++h)
 				if (Trinatri(a, i, j, h) || Horizont(a, j, h) || Vertical(a, i, h))
 					--c;
-			if (!c)//не c
+			if (!c)
 			{
 				InitializationMas(a);
 				Sudoku(a);
@@ -73,79 +73,20 @@ void ShowMas(int** a)
 	{
 		for (int j = 0; j < 9; ++j)
 			
-			printf("%3d", a[i][j]);//распечатать испольщу€ 3 пробела и переремена€ целого типа(int)
+			printf("%3d", a[i][j]);
 		printf("\n\n");
 
 	}
 	
 
 }
-void gotoxy(int x,int y)
-{
-	COORD coord;
-	coord.X = x;
-	coord.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-
-}
-void showTable( int x, int y) {
-	SetConsoleCP(866);
-	SetConsoleOutputCP(866);
-
-	gotoxy(x, y);
-	cout << char(201);
-	for (int i = 0; i < 5; i++) cout << char(205);
-	cout << char(209);
-	for (int i = 0; i < 5; i++) cout << char(205);
-	cout << char(209);
-	for (int i = 0; i < 5; i++) cout << char(205);
-	cout << char(187);
-
-	gotoxy(x, ++y);
-	for (int i = 0; i < 9; i++) {
-		if (i == 3 || i == 6) {
-			cout << char(199);
-			for (int i = 0; i < 5; i++) cout << char(196);
-			cout << char(197);
-			for (int i = 0; i < 5; i++) cout << char(196);
-			cout << char(197);
-			for (int i = 0; i < 5; i++) cout << char(196);
-			cout << char(182);
-			gotoxy(x, ++y);
-		}
-		cout << char(186);
-		for (int j = 0; j < 9; j++) {
-			
-			/*if (Sudoku[j][i].getVal() > 0)
-				cout << Sudoku[j][i].getVal();
-			else*/
-				cout << ' ';
-			if (j != 2 && j != 5 && j != 8)
-				cout << ' ';
-			if (j == 2 || j == 5)
-				cout << char(179);
-		}
-		cout << char(186);
-		gotoxy(x, ++y);
-	}
-
-	cout << char(200);
-	for (int i = 0; i < 5; i++) cout << char(205);
-	cout << char(207);
-	for (int i = 0; i < 5; i++) cout << char(205);
-	cout << char(207);
-	for (int i = 0; i < 5; i++) cout << char(205);
-	cout << char(188);
-	gotoxy(x, ++y);
-	cout << endl;
-}
 
 int main()
 {
 
-	int** a = (int **)calloc(9, sizeof(int *));//выдел€ем пам€ть
+	int** a = (int **)calloc(9, sizeof(int *));
 	for (int i = 0; i < 9; ++i)
-		a[i] = (int *)calloc(9, sizeof(int));// заполн€ем 
+		a[i] = (int *)calloc(9, sizeof(int));
 	InitializationMas(a);
 	Sudoku(a);
 	ShowMas(a);
